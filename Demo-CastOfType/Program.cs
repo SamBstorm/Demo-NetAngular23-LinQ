435b5d2360d9ee6f7d6b3eb482270451ed1e5bcb
Samuel Legrain <samuel.legrain@bstorm.be>
2023-03-28 12:27:45 +0200

    Add Demo-CastTypeOf

---
+++ Demo-CastOfType/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+ArrayList list = new ArrayList() { 1,42,33,"Toto",'a',123 };
+
+IEnumerable<int> ints_cast = list.Cast<int>(); //Déclanche une Exception unable to Cast in INT32
+IEnumerable<int> ints_type = list.OfType<int>();
+
+foreach (int i in ints_type)
+{
+    Console.WriteLine($"{i} - {i.GetType()}");
+}
